6b6da01b9c8fb04669b336731f00f1ae8e98d27b
Trandaf <trandafgaming@gmail.com>
2024-03-19 12:34:07 +0200

    Version 1
    Prima versiune :)

--- obj/Debug/net8.0-windows/Calculator PI.AssemblyInfo.vb
+++ obj/Debug/net8.0-windows/Calculator PI.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Calculator PI"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbf8ba647a92717c82eb4b5461801d15c0ddcc2"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a83882025274fd92a186c3c23d80f22ae5c687"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Calculator PI"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Calculator PI"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
